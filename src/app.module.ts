3614cf832db143b67b22c28542fd9b3a63ad14ac
Gabriel Diaz <gabidiaz4231@gmail.com>
2025-06-19 18:56:33 -0300

    Estructuración del proyecto

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { Controlador2Controller } from './controlador2/controlador2.controller';
+import { UsersModule } from './users/users.module';
+import { RolesModule } from './roles/roles.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, RolesModule],
   controllers: [AppController, UsersController, Controlador2Controller],
   providers: [AppService],
 })
--- src/controlador2/controlador2.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('controlador2')
-export class Controlador2Controller {}
