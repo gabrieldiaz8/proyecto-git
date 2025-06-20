2940770a2a6b3a170095d108fa807f1c1bfb4267
ImanolBarrionuevo <barrionuevoimanol@gmail.com>
2025-06-20 17:59:11 -0300

    Cambio de puerto

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000); // Query Params
+  await app.listen(process.env.PORT ?? 3001); // Query Params
 }
 bootstrap();
